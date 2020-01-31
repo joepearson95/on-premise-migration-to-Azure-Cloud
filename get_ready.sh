0644917b9a3ad4cb2e9b6258845ebd7d9e6445ac
Joe Pearson <joe.c.pearson95@gmail.com>
2020-01-31 10:43:41 +0000

    Simple app setup.

--- get_ready.sh
+++ get_ready.sh
@@ -1,12 +1,10 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-mkdir flask_app
-cd flask_app
-touch application.py
-touch requirements.txt
+cd cloud_site
 
 python3 -m venv dissVM
 source venv/bin/activate
 pip install -r requirements
 export FLASK_APP=application.py
+flask run
