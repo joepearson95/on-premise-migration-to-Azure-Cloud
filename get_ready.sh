b5cd33e8c685bdcced511de3a4f0ccb53a80988e
Joe Pearson <joe.c.pearson95@gmail.com>
2020-01-31 13:08:27 +0000

    Updated bash and IaaS code

--- get_ready.sh
+++ get_ready.sh
@@ -5,7 +5,7 @@ sudo apt-get install python3-venv
 cd cloud_site
 
 python3 -m venv dissVM
-source venv/bin/activate
+source dissVM/bin/activate
 pip install -r requirements
 export FLASK_APP=application.py
 flask run
