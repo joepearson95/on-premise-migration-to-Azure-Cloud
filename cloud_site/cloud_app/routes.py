ad98503486eac8886abcfbbee5ad26e277bd5752
Joe Pearson <joe.c.pearson95@gmail.com>
2020-02-04 16:09:57 +0000

    Adjusting the actual website now.

--- cloud_site/cloud_app/routes.py
+++ cloud_site/cloud_app/routes.py
@@ -1,24 +1,49 @@
+from datetime import datetime
 from flask import render_template, url_for, flash, redirect, request
 from cloud_app import app, db, bcrypt
 from cloud_app.forms import RegistrationForm, LoginForm
 from cloud_app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
+@app.context_processor
+def inject_now():
+	return {'now': datetime.utcnow()}
+
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash('Your account has been created! You are now able to log in', 'success')
+		return redirect(url_for('login'))
 	return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check email and password', 'danger')
 	return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout():
 	logout_user()
-	return redirect(url_for('/'))
+	return redirect(url_for('home'))
