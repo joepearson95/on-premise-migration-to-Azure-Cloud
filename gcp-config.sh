0a5af999ff59ab609b8d283046c3b9c64fc16648
Joe Pearson <joe.c.pearson95@gmail.com>
2020-02-06 14:42:42 +0000

    adjusting gcp config

--- gcp-config.sh
+++ gcp-config.sh
@@ -8,4 +8,8 @@ sudo apt-get -y install python-setuptools python-dev build-essential
 sudo easy_install pip
 sudo pip install flask
 
-sed -i '15,16 s/^/#/' cloud_app/models.py
+sed -i '15,16 s/^/#/' cloud_site/cloud_app/models.py
+sed -i '17 s/^/#/' cloud_site/requirements.txt
+cd cloud_site
+sudo pip install -r requirements.txt
+sudo python gcp-run.py
