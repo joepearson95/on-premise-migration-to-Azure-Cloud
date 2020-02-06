4a94c89d13c4a4fddcd4df860bfd9b9773783b61
Joe Pearson <joe.c.pearson95@gmail.com>
2020-02-06 14:10:20 +0000

    Adding GCP config.

---
+++ gcp-config.sh
@@ -0,0 +1,9 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install python3-venv
+sudo apt-get -y install python3-dev
+sudo apt-get -y install nginx
+sudo apt-get -y install git
+sudo apt-get -y install python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo pip install flask
